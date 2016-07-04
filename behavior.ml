588c579e2d7f5eff34bb9cdcb5cf7d7e30e2ebd7
KC Sivaramakrishnan <sk826@cl.cam.ac.uk>
2016-07-04 07:50:08 +0100

    More work on behaviour

--- behavior.ml
+++ behavior.ml
@@ -1,5 +1,8 @@
 (* Fun with behavioral types. Inspired by https://hal.archives-ouvertes.fr/hal-01216310 *)
 
+exception LinearityViolation
+let (|>) v f = f v
+
 module type Ref =
 sig
   type ('a, 'b) ref constraint 'b = [>]
@@ -9,9 +12,7 @@ sig
   val read  : ('a, [`Read of 'b]) ref -> 'a * ('a, 'b) ref
   (* Require `Read capability. 'b is the continuation's behavior. *)
   val write : ('a, [`Write of 'b]) ref -> 'a -> ('a, 'b) ref
-
-  val left : ('a, [`Left of 'b | `Right of 'c]) ref -> ('a, 'b) ref
-  val right : ('a, [`Left of 'b | `Right of 'c]) ref -> ('a, 'c) ref
+  val branch : ('a, [>] as 'b) ref -> (('a, [>] as 'c) ref -> 'b) -> ('a, 'c) ref
 end
 
 module Ref : Ref =
@@ -24,18 +25,21 @@ struct
 
   let ref v = {contents = v; live = true}
 
+  let check r =
+    if not r.live then raise LinearityViolation;
+    r.live <- false
+
+  let fresh r = {r with live = true}
+
   let read r =
-    assert (r.live);
-    r.live <- false;
-    (r.contents, { r with live = true })
+    check r;
+    (r.contents, fresh r)
 
   let write r v =
-    assert (r.live);
-    r.live <- false;
+    check r;
     { contents = v; live = true }
 
-  let left {contents; live} = {contents; live}
-  let right {contents; live} = {contents; live}
+  let branch r _ = check r; fresh r
 end
 
 (* This reference can only be written once followed by a single read *)
@@ -82,24 +86,195 @@ let my_ref3 : (int, [`Write of [`Read of [>]]]) Ref.ref = Ref.ref 10
 (* let _ = foo2 my_ref3 (* foo2 accepts my_ref3, and runs forever! *) *)
 
 let rec foo3 r = function
-  | 0 -> Ref.write (Ref.left r) 0
+  | 0 ->
+      print_endline "done";
+      Ref.write (Ref.branch r (fun x -> `Zero x)) 0
   | n ->
-      let r = Ref.write (Ref.right r) 20 in
+      let r = Ref.write (Ref.branch r (fun x -> `Succ x)) 20 in
       let v, r = Ref.read r in
       foo3 r (n-1)
 
 (* val foo3 :
- *   (int,
- *    [ `Test of [ `Write of [>  ] as 'b ] * [ `Write of [ `Read of 'a ] ] ] as 'a)
- *   Ref.ref -> int -> (int, 'b) Ref.ref
+ *  (int,
+ *   [> `Succ of (int, [ `Write of [ `Read of 'a ] ]) Ref.ref
+ *    | `Zero of (int, [ `Write of [>  ] as 'b ]) Ref.ref ]
+ *  as 'a)
  *
- * The inferred type accounts for the fact that the branches do different
- * actions. However, this interface isn't great; I don't like the fact that the
- * reference needs to be threaded through. It might be troublesome in the presence
- * of multiple references.
+ * The inferred type captures branching behavior. The argument to the branch is
+ * always of the form << fun x -> `Tag x >>.
  *)
-
 
 let my_ref4 = Ref.ref 10
 let _ = foo3 my_ref4 32
 (* Works as expected *)
+
+module type File_descriptor = sig
+  type 'a t constraint 'a = [>]
+
+  val openfile : string -> Unix.open_flag list -> Unix.file_perm -> ([< `Close | `Write of 'a | `Read of 'a > `Close] as 'a) t
+  val close : [> `Close] t -> unit
+  val read : [> `Read of 'a] t -> bytes -> int -> int -> int * 'a t
+  val write : [> `Write of 'a] t -> bytes -> int -> int -> int * 'a t
+  val mk_read_only  : [> `Read of 'a] t -> ([`Close | `Read of 'a] as 'a) t
+  val mk_write_only : [> `Write of 'a] t -> ([`Close | `Write of 'a] as 'a) t
+
+  val open_stdin  : unit -> ([`Close | `Read of 'a] as 'a) t
+  val open_stdout : unit -> ([`Close | `Write of 'a] as 'a) t
+  val open_stderr : unit -> ([`Close | `Write of 'a] as 'a) t
+end
+
+module File_descriptor : File_descriptor = struct
+  open Unix
+
+  type 'a t =
+    {fd : file_descr;
+     mutable live : bool} constraint 'a = [>]
+
+  let mk fd = {fd = fd; live = true}
+
+  let fresh fd = {fd with live = true}
+
+  let check fd =
+    if not fd.live then raise LinearityViolation;
+    fd.live <- false
+
+  let open_stdin () = mk stdin
+  let open_stdout () = mk stdout
+  let open_stderr () = mk stderr
+
+  let openfile file flags perm =
+    let fd = openfile file flags perm in
+    mk fd
+
+  let close fd = check fd; close fd.fd
+
+  let read fd buff ofs len =
+    check fd;
+    (read fd.fd buff ofs len, fresh fd)
+
+  let write fd buff ofs len =
+    check fd;
+    (write fd.fd buff ofs len, fresh fd)
+
+  let mk_read_only fd = check fd; fresh fd
+  let mk_write_only fd = check fd; fresh fd
+end
+
+module type Alias = sig
+  type ('a,'b) t constraint 'b = [>]
+  val make   : (unit -> 'a) -> ('a, [`One]) t
+  val dup    : ('a, 'b) t -> ('a,[`Succ of 'b]) t * ('a, [`Succ of 'b]) t
+  val merge  : ('a, [`Succ of 'b]) t -> ('a, [`Succ of 'b]) t -> ('a, 'b) t
+  val delete : ('a, [`One]) t -> ('a -> unit) -> unit
+  val app   : ('a,'b) t -> ('a -> unit) -> unit
+end
+
+module Alias : Alias = struct
+  type ('a,'b) t =
+    {v : 'a; mutable live : bool} constraint 'b = [>]
+
+  let fresh a = {a with live = true}
+
+  let check a =
+    if not a.live then raise LinearityViolation;
+    a.live <- false
+
+  let make f = {v = f (); live = true}
+  let dup x = check x; (fresh x, fresh x)
+  let merge x y = check x; check y; fresh x
+  let delete x f = check x; f x.v
+  let app x f = f x.v
+end
+
+module type PolyRef =
+sig
+  type 'a ref constraint 'a = [>]
+  val ref   : 'a -> ([`Read of 'a * 'b | `Write of 'c * 'd] as 'b) ref
+  val read  : [> `Read  of 'a * ([< `Read of 'a * 'b | `Write of 'c * 'd] as 'b)] ref -> 'a * 'b ref
+  val write : [> `Write of 'a * ([< `Read of 'a * 'b | `Write of 'c * 'd] as 'b)] ref -> 'a -> 'b ref
+end
+
+module PolyRef : PolyRef =
+struct
+  type 'a ref =
+    {contents     : 'b. 'b;
+     mutable live : bool} (* For linearity *)
+     constraint 'a = [>]
+
+  let ref v = {contents = Obj.magic v; live = true}
+
+  let check r =
+    if not r.live then raise LinearityViolation;
+    r.live <- false
+
+  let fresh r = {r with live = true}
+
+  let read r =
+    check r;
+    (Obj.magic r.contents, fresh r)
+
+  let write r v =
+    check r;
+    { contents = Obj.magic v; live = true }
+
+end
+
+let foo_polyref r =
+  let open PolyRef in
+  let v,r = read r in
+  Printf.printf "%d\n" v;
+  let v,r = read r in
+  Printf.printf "%d\n" v;
+  let r = write r "hello" in
+  let v,r = read r in
+  print_endline v
+
+module type PolyRef2 =
+sig
+  type ('a,'b) rw_prot constraint 'b = [> `Read of 'a * 'b | `Write of 'c * ('c,_) rw_prot]
+  type 'a ref constraint 'a = ('b,'c) rw_prot
+  val ref  : 'a -> ('a,'b) rw_prot ref
+  val read  : ('a,[> `Read of 'a * 'b]) rw_prot ref -> 'a * ('a,'b) rw_prot ref
+  val write : ('a,[> `Write of 'b * ('b,'c) rw_prot]) rw_prot ref -> 'b -> ('b,'c) rw_prot ref
+  val branch : ('a, [>] as 'b) rw_prot ref -> (('a, [>] as 'c) rw_prot ref -> 'b) -> ('a, 'c) rw_prot ref
+end
+
+module PolyRef2 : PolyRef2 =
+struct
+  type ('a,'b) rw_prot constraint 'b = [> `Read of 'a * 'b | `Write of 'c * ('c,_) rw_prot]
+
+  type 'a ref =
+    {contents     : 'b.'b;
+     mutable live : bool} (* For linearity *)
+     constraint 'a = ('b,'c) rw_prot
+
+  let ref v = {contents = Obj.magic v; live = true}
+
+  let check r =
+    if not r.live then raise LinearityViolation;
+    r.live <- false
+
+  let fresh r = {r with live = true}
+
+  let read r =
+    check r;
+    (Obj.magic r.contents, fresh r)
+
+  let write r v =
+    check r;
+    { contents = Obj.magic v; live = true }
+
+  let branch r _ = check r; fresh r
+end
+
+let foo_polyref2 r =
+  let open PolyRef2 in
+  let v,r = read r in
+  Printf.printf "%d\n" v;
+  let v,r = read r in
+  Printf.printf "%d\n" v;
+  let r = write r "hello" in
+  let v,r = read r in
+  print_endline v
+
+
