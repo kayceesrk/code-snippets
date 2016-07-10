d86e1bc9152d7d31cf15a879fdb790a3f180bcdc
KC Sivaramakrishnan <sk826@cl.cam.ac.uk>
2016-07-10 23:25:58 +0100

    Tree shape works

---
+++ tree_shape.ml
@@ -0,0 +1,135 @@
+module type Tree =
+sig
+  type ('a,+'b) tree constraint 'b = [>]
+
+  val mk_leaf  : unit -> ('a,[`Leaf]) tree
+  val mk_node  : ('a, 'b) tree -> 'a -> ('a, 'c) tree -> ('a, [`Node of 'b * 'c ]) tree
+
+  val value : ('a, [`Node of _] as 'b) tree -> 'a * ('a, 'b) tree
+  val left  : ('a, [`Node of 'b * _] as 'd) tree ->
+    ('a, 'b) tree * ('a, 'd) tree
+  val right : ('a, [`Node of _ * 'b] as 'd) tree ->
+    ('a, 'b) tree * ('a, 'd) tree
+  val destruct : ('a, [< `Leaf | `Node of 'b * 'c]) tree ->
+    [`L | `N of ('a,[`Node of 'b * 'c]) tree]
+  val lift : ('a, [< `Leaf | `Node of 'b * 'c]) tree ->
+    ('a, [`Leaf | `Node of 'b * 'c]) tree
+  val destruct2 : ('a, [`Leaf | `Node of 'b * 'c]) tree ->
+    [`L | `N of ('a,[`Node of 'b * 'c]) tree]
+
+end
+
+module Tree : Tree =
+struct
+  type 'a _tree = Leaf | Node of 'a _tree * 'a * 'a _tree
+  type ('a,+'b) tree = ('a _tree * bool ref) constraint 'b = [>]
+
+  let fresh x = (x, ref true)
+  exception LinearityViolation
+  let check r = if not !r then raise LinearityViolation else r := false
+
+  let mk_leaf () = fresh Leaf
+  let mk_node (l,s1) v (r,s2) =
+    check s1; check s2;
+    fresh (Node (l, v, r))
+
+  let value (t,r) =
+    check r;
+    match t with
+    | Leaf -> failwith "impossible"
+    | (Node (_,v,_)) as n -> (v, fresh n)
+
+  let left (t,r) =
+    check r;
+    match t with
+    | Leaf -> failwith "impossible"
+    | (Node (l,_,_)) as n -> (fresh l, fresh n)
+
+  let right (t,r) =
+    check r;
+    match t with
+    | Leaf -> failwith "impossible"
+    | (Node (_,_,r)) as n -> (fresh r, fresh n)
+
+  let destruct (t,r) =
+    check r;
+    match (Obj.magic t) with
+    | Leaf -> `L
+    | Node (l,v,r) -> `N (fresh (Node (l,v,r)))
+
+  let construct t = t
+  let lift t = t
+  let destruct2 = destruct
+end
+
+open Tree
+
+let rec preorder t f =
+  match destruct t with
+  | `L -> ()
+  | `N t ->
+      let v, t = value t in
+      f v;
+      let l , t = left t in
+      preorder (Obj.magic l) f;
+      let r, t = right t in
+      preorder (Obj.magic r) f
+
+let make_tree1 () =
+  mk_node
+    (mk_leaf ())
+    10
+    (mk_node
+      (mk_leaf ())
+      20
+      (mk_leaf ())
+    )
+
+
+let make_tree2 () =
+  mk_node
+    (mk_node
+      (mk_leaf ())
+      10
+      (mk_leaf ()))
+    10
+    (mk_node
+      (mk_leaf ())
+      20
+      (mk_leaf ()))
+
+let make_tree3 () =
+  mk_node
+    (mk_node
+      (mk_node
+        (mk_leaf ())
+        40
+        (mk_leaf ()))
+      10
+      (mk_leaf ()))
+    10
+    (mk_node
+      (mk_leaf ())
+      20
+      (mk_leaf ()))
+
+module Balanced = struct
+  type ('a,+'b) balanced_tree = ('a,'b) tree
+    constraint 'b = [< `Leaf | `Node of 'c * 'c]
+end
+
+module RightBranching = struct
+  type ('a,+'b) right_branching = ('a,'b) tree
+    constraint 'b = [`Leaf | `Node of [`Leaf] * 'b]
+end
+
+module LeftBranching = struct
+  type ('a,+'b) left_branching = ('a,'b) tree
+     constraint 'b = [`Leaf | `Node of 'b * [`Leaf]]
+end
+
+let _ = preorder (make_tree2 ()) (fun x -> Printf.printf "%d\n" x)
+let x : (_,_) Balanced.balanced_tree = make_tree2 ()
+let y : (_,_) RightBranching.right_branching =
+  (make_tree1 () : (('a,[`Node of [`Leaf] * [`Node of [`Leaf] * [`Leaf]]]) tree) :>
+                   (('a,[`Leaf | `Node of [`Leaf] * 'b] as 'b) tree))
