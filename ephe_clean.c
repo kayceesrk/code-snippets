06c838cde50e1ea434f5945dbb85a08e4e9c9dd5
KC Sivaramakrishnan <kc@kcsrk.info>
2018-05-22 11:17:27 +0100

    Fix key_read

--- ephe_clean.c
+++ ephe_clean.c
@@ -1,5 +1,7 @@
 /* Assume that the ephemeron is only composed of a single key and data.
- * Mutator can concurrently modify the keys and data. */
+ * Mutator can concurrently modify the keys and data.
+ *
+ * TODO: Considers only minor GCs. Expand to cover major GC. */
 
 typedef struct {
   value key;
@@ -14,26 +16,26 @@ typedef struct {
 } ephe_clean_descriptor_t;
 
 /* Clean the data for dead key. Has a single linearlization point. */
-int ephe_clean (ephe_clean_descriptor_t *d) {
+void ephe_clean (ephe_clean_descriptor_t *d) {
   assert (Is_dead(d->old_key));
   d->old_data = *(d->data);
   /* d->data might have been updated by the mnutator in the mean time. As long
    * as the key remains the old_key, the new data will be released. */
   if (!__sync_bool_compare_and_swap (d->data, d->old_data, d)) {
     /* data updated. try again to avoid losing writes to data. */
-    ephe_clean(d);
+    return ephe_clean(d);
   }
   k = *(d->key);
   if (k == d->old_key) {
     if (!__sync_bool_compare_and_swap (d->data, d, None_val)) { //Linearization point.
       /* data updated. try again to avoid losing writes to data. */
-      ephe_clean(d);
+      return ephe_clean(d);
     }
   } else {
     /* Key has been updated. Clean should be a noop. Try to change the data
      * back to original one. OK for CAS to fail since we need to avoid losing
      * writes to data. */
-    __sync_bool_compare_and_swap (d->data, d, d->old_val);
+    __sync_bool_compare_and_swap (d->data, d, d->old_data);
   }
 }
 
@@ -46,13 +48,17 @@ int set_data (ephemeron_t* e, value d) {
 }
 
 int get_key (ephemeron_t* e) {
+  if (Is_foreign(e->key)) {
+    read_fault(e->key);
+  }
   return e->key;
 }
 
 int get_data (ephemeron_t* e) {
   d = e->data;
   if (Is_descriptor(d)) {
-    return d->data;
+    ephe_clean(d);
+    return get_data(e);
   }
   return d;
 }
