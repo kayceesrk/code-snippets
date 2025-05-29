600d90ec6ae8ecbdb6be9f65409619726cc9e062
KC Sivaramakrishnan <kc@kcsrk.info>
2025-05-29 15:33:38 +0530

    updates

---
+++ unique_ref.ml
@@ -0,0 +1,19 @@
+module type S = sig
+  type 'a t
+  val alloc : 'a -> 'a t @ unique
+  val free : 'a t @ unique -> unit
+  val get : 'a t @ unique -> 'a Modes.Aliased.t * 'a t @ unique
+  val set : 'a t @ unique -> 'a -> 'a t @ unique
+end
+
+module M : S = struct
+  type 'a t = { mutable value : 'a }
+  let alloc x = unique_ { value = x }
+  let free t = ()
+  let get (t @ unique) : _ Modes.Aliased.t * _ @ unique =
+    let a = Modes.Aliased.{aliased = t.value } in
+    a, t
+  let set (t @ unique) x =
+    t.value <- x;
+    t
+end
